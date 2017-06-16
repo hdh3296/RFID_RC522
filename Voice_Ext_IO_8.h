83ff633fa3661cd97a29f59cf963aff9051cf96b
hdh3296 <hdh3296@gmail.com>
2017-06-16 14:51:38 +0900

    프로젝트 생성
    VOICE 펌웨어 프로젝트를 복사해와서 프로젝트 만들었다.
    
    SPI통신을 통해 RC_522 보드와 통신하기 위한 목적으로 개발한다.
    
    Signed-off-by: hdh3296 <hdh3296@gmail.com>

---
+++ Voice_Ext_IO_8.h
@@ -0,0 +1,24 @@
+
+
+#include    <pic18.h>
+
+
+
+#include        "RC_system_com\memory_map.h"
+#include        "RC_comm_4480\iodef.h"
+#include        "RC_comm_4480\fontout.h"
+#include        "RC_comm_4480\comdata.h"
+#include        "RC_comm_4480\keysort.h"
+#include        "RC_comm_4480\setup.h"
+#include        "RC_comm_4480\esdots.h"
+
+
+
+extern	void    		Ext_IO_8_Init(void);
+extern	unsigned int  	Ext_IO_8_Func(void);
+extern	void			Ext_IO_8_Timer_1msec(void);
+
+
+
+
+
